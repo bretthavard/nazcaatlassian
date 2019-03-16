ac72c6657edfc4e25ff559c9ba5ccf2d66cfe241
nazcasites <brett@nazca.com.au>
2019-03-16 09:13:05 +0000

    jira2

---
+++ jira/jirapatch.sh
@@ -0,0 +1,6 @@
+cp /opt/atlassian/jira/atlassian-jira/WEB-INF/atlassian-bundled-plugins/atlassian-universal-plugin-manager-plugin-2.22.9.jar
+ /opt/atl/atlassian-bundled-plugins/atlassian-universal-plugin-manager-plugin-2.22.9.jar
+
+cp /opt/atl/jira/lib/atlassian-extras-3.2.jar /opt/atlassian/jira/atlassian-jira/WEB-INF/lib/atlassian-extras-3.2.jar
+
+cp /opt/atl/jira/lib/atlassian-extras-3.2.jar /opt/atlassian/jira/temp/plugin.7961617972122759374.atlassian-bonfire-plugin-2.9.18.00034.obr
